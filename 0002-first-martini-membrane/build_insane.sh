e919c1dc0d4bda76ab734e1366154cad23753f30
Jarvist Moore Frost <jarvist@gmail.com>
2024-08-21 18:04:33 +0100

    Notes on what I did, for the future.

--- 0002-first-martini-membrane/build_insane.sh
+++ 0002-first-martini-membrane/build_insane.sh
@@ -1,2 +1,11 @@
 insane -x 20 -y 20 -z 10 -l DPPC:50 -l DOPC:50  -sol W -salt 0.15 -rand 0.095 -solr 0.5 -o symmetric-bilayer.gro -p symmetric-bilayer.top
 
+# suggests Na+ and Cl- as names; but Martini 3 calls these Na and Cl
+# It seems that GROMACS is clever enough to follow the naming
+#
+# So probably need to delete the - and + from the symmetric-bilayer.top as
+# a post-process step
+
+# At this point in time, I did this by hand-editing the symmetric-bilayer.top
+# file
+
