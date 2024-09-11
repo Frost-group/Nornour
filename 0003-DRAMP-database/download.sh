f2769f6f2e814e1e654fba29e78bc5ab57ae9fcd
Jarvist Moore Frost <jarvist@gmail.com>
2024-09-11 10:56:50 +0100

    Sequences downloaded and extracted from the DRAMP database.

---
+++ 0003-DRAMP-database/download.sh
@@ -0,0 +1,13 @@
+# The DRAMP database offers perfectly formatted downloads of the data
+# http://dramp.cpu-bioinfor.org/downloads/
+#
+# *Citation*:
+# Shi G, Kang X, Dong F, Liu Y, Zhu N, Hu Y, Xu H, Lao X, Zheng H. DRAMP 3.0:
+# an enhanced comprehensive data repository of antimicrobial peptides. Nucleic
+# Acids Res. 2022 Jan 7;50(D1):D488-D496. PMID: 34390348
+
+# (づ ᴗ _ ᴗ) づ ♡ - I love a good simple URL download
+# wget "http://dramp.cpu-bioinfor.org/downloads/download.php?filename=download_data/DRAMP3.0_new/Antibacterial_amps.txt" -O Antibacterial_amps.txt
+
+cat Antibacterial_amps.txt | grep "^DRAMP" | awk '{print $2}' > Antibacterial_amps_sequences.txt
+
