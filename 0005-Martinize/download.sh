0501e6c2d48d655705fea1293e98ee27ee4e30b4
Jarvist Moore Frost <jarvist@gmail.com>
2024-09-17 21:49:21 +0100

    Discoverted Martinize 2; update info in download.sh as readme

--- 0005-Martinize/download.sh
+++ 0005-Martinize/download.sh
@@ -1,5 +1,12 @@
-wget https://cgmartini-library.s3.ca-central-1.amazonaws.com/1_Downloads/tools/proteins_and_bilayers/martinize_v_2_6_py3.py
+# Martinize is the process of coverting atomistic structures into CG Martini representions
+#  i.e. from an atomistic representation of a peptide to the Martini beads
 
+# Martinize 1
+# wget https://cgmartini-library.s3.ca-central-1.amazonaws.com/1_Downloads/tools/proteins_and_bilayers/martinize_v_2_6_py3.py
 # Also on GitHub, but this is the latest version & concatenated together so you
 # can run directly.
+# Such an ancient version of Python 3, it needs to have removed `print "string"` statements rewritten as functions.
 
+# Martinize 2
+# go-Martini & etc. are incorporated into Martinize 2
+https://github.com/marrink-lab/vermouth-martinize?tab=readme-ov-file
