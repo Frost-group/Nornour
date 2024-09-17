2457cf164a9738f94fc44c4af5c27ef17cbfb84e
Jarvist Moore Frost <jarvist@gmail.com>
2024-09-17 22:49:42 +0100

    XTB for the win.

--- 0005-Martinize/simulate_peptide.sh
+++ 0005-Martinize/simulate_peptide.sh
@@ -6,11 +6,14 @@ for peptide in ` cat peptides.dat `
 do
     echo "fab ${peptide}; save ${peptide}.pdb" | pymol -c -p
 
-    gmx pdb2gmx -ignh  -f ${peptide}.pdb
+#    gmx pdb2gmx -ignh  -f ${peptide}.pdb
+#    gmx solvate -box 5 5 5 -cp conf.gro -p topol.top
+#    gmx grompp -f emin-charmm.mdp -c out.gro
 
-    gmx solvate -box 5 5 5 -cp conf.gro -p topol.top
-
-    gmx grompp -f emin-charmm.mdp -c out.gro
+# Hold my beer
+# Simulate protein MD with XTB (empirical tight-binding) model; uses Langevin
+# dynamics and an implicit water model
+ xtb --gfn 2 --md ${peptide}.pdb
 
 done
 
