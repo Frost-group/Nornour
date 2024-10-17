e1f21814515c5c4e7434a5194502ddc765b14fb3
Igor Gonteri <160867706+igorgonteri@users.noreply.github.com>
2024-10-17 15:24:25 +0100

    Create download.sh

---
+++ 0003c-APD-Database/download.sh
@@ -0,0 +1,3 @@
+# source "Wang, G., Li, X. and Wang, Z. (2016) APD3: the antimicrobial peptide database as a tool for research and education. Nucleic Acids Research 44, D1087-D1093"
+
+# raw file https://aps.unmc.edu/assets/sequences/APD_sequence_release_09142020.fasta
