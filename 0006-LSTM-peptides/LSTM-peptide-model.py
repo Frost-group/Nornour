e72d2cd9f7e67742046d4c653e3ea473df04088e
Igor Gonteri <igor.gonteri@epfl.ch>
2024-10-23 22:37:16 +0100

    Coding of LSTM model training and testing loops

---
+++ 0006-LSTM-peptides/LSTM-peptide-model.py
@@ -0,0 +1,212 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+import math
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+
+class LSTMArgs:
+    def __init__(self):
+        self.output_size = 22
+        self.num_epochs = 100
+        self.batch_size = 64
+        self.learning_rate = 0.001
+        self.hidden_size = 128
+        self.layers = 2
+
+
+def open_file(filepath):
+    with open(filepath, 'r') as doc:
+        peptides = doc.readlines()
+        long_pep = max(len(pep.strip()) for pep in peptides)
+
+    return peptides, long_pep
+
+
+def padding(peptides, long_pep):
+    for i, pep in enumerate(peptides):
+        pad_length = long_pep - len(pep)
+
+    if pad_length > 0:
+        peptides[i] = pep.strip() + '_' * pad_length
+
+    return peptides
+
+
+def split_data(peptides):
+    len_pep = len(peptides)
+    train_tresh = math.floor(len_pep * 0.85)
+
+    train_data = peptides[:train_tresh]
+    test_data = peptides[train_tresh:]
+
+    return train_data, test_data
+
+
+def to_tensor(peptides, vocab):
+    to_index = {a: i for i, a in enumerate(vocab)}
+    result = []
+
+    for pep in peptides:
+        # Replace invalid characters with a default (e.g., index of '_')
+        result.append([to_index.get(aa, to_index['_']) for aa in pep.strip()])
+
+    return torch.tensor(result), to_index
+
+
+def to_amino(tensor, vocab):
+    sequences = []
+    for seq in tensor:
+        amino_seq = ''.join([vocab[idx] for idx in seq])
+        sequences.append(amino_seq)
+    return sequences
+
+
+def create_batches(tensor, batch_size):
+    data_size = len(tensor)
+
+    indices = list(range[data_size])
+    random.shuffle(indices)
+
+    for start in range(0, data_size, batch_size):
+        end = min(start + batch_size, data_size)
+        batch_indices = indices[start: end]
+        batch = tensor[batch_indices]
+
+    return batch
+
+
+class LSTMPeptides(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, batch_size, layers=2):
+        super(LSTMPeptides, self).__init__()
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.lstm_size = 128
+        self.output_size = output_size
+        self.batch_size = batch_size
+        self.layers = layers
+
+        self.vocab = ['R', 'H', 'K', 'D', 'E', 'S', 'T', 'N', 'Q', 'C',
+                      'U', 'G', 'P', 'A', 'I', 'L', 'M', 'F', 'W', 'Y', 'V']
+        self.len_vocab = len(self.vocab)
+
+        self.embedding_dim = 128
+        self.embedding = nn.Embedding(num_embeddings=self.len_vocab, embedding_dim=self.embedding_dim)
+
+        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, prev_state):
+
+        embed = self.embedding(x)
+        output, state = self.lstm(embed, prev_state)
+        logits = self.fc(output)
+        return logits, state
+
+    def init_state(self, sequence_length):
+
+        return (torch.zeros(self.layers, sequence_length, self.lstm_size),
+                torch.zeros(self.layers, sequence_length, self.lstm_size))
+
+
+
+
+
+def train(peptides, model, args):
+    model.train()
+
+    vocab = ['R', 'H', 'K', 'D', 'E', 'S', 'T', 'N', 'Q', 'C', 'U', 'G', 'P', 'A', 'I', 'L', 'M', 'F', 'W',
+             'Y', 'V', '_']
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+
+    inp_peptides = to_tensor(peptides, vocab)
+    seq_len = len(peptides)
+
+    batch_size = args.batch_size
+
+    print('Starting Training \n -----------------------------')
+
+    for epoch in range(args.num_epochs):
+        state_h, state_c = model.init_state(seq_len)
+
+        for batch in create_batches(inp_peptides, batch_size):
+            optimizer.zero_grad()
+
+            x = batch[:, :-1].to(device)
+            y = batch[:, 1:].to(device)
+
+            y_pred, (state_h, state_c) = model(x, (state_h, state_c))
+            loss = criterion(y_pred.transpose(1, 2), y)
+
+            state_h = state_h.detach()
+            state_c = state_c.detach()
+
+            loss.backward()
+            optimizer.step()
+
+            print({'epoch': epoch, 'loss': loss.item()})
+
+        print('End of Training \n -----------------------------')
+
+def test(test_data, model, sequence_length, batch_size):
+
+    model.eval()
+    test_loss, correct = 0, 0
+    num_batches = len(test_data) // batch_size
+    criterion = nn.CrossEntropyLoss()
+
+    print('Starting Testing \n -----------------------------')
+
+    with torch.no_grad():
+        state_h, state_c = model.init_state(sequence_length)
+
+        for i in range(0, len(test_data), batch_size):
+            X = test_data[i:i + batch_size]
+            X = X.to(device)
+
+            y = X[:, 1:].to(device)  # Target is the next sequence
+            X = X[:, :-1].to(device)  # Input is the current sequence
+
+            y_pred, (state_h, state_c) = model(X, (state_h, state_c))
+
+            test_loss += criterion(y_pred.transpose(1, 2), y).item()
+
+            correct += (y_pred.argmax(2) == y).type(torch.float).sum().item()
+
+            state_h = state_h.detach()
+            state_c = state_c.detach()
+
+
+    test_loss /= num_batches
+    accuracy = correct / (len(test_data) * (sequence_length - 1))  # Adjust based on sequence length
+
+    print(f'Test Error: \n Accuracy: {100 * accuracy:.2f}%, Avg loss: {test_loss:.4f} \n')
+
+    print('End of testing \n -----------------------------')
+    return test_loss, accuracy
+
+
+vocab = ['R', 'H', 'K', 'D', 'E', 'S', 'T', 'N', 'Q', 'C', 'U',
+         'G', 'P', 'A', 'I', 'L', 'M', 'F', 'W', 'Y', 'V', '_']
+len_vocab = len(vocab)
+
+args = LSTMArgs()
+
+dataset = '/Users/igorgonteri/Documents/GitHub/Nornour/0003c-APD-Database/antimicrobial_peptides_database.txt'
+peptides, long_pep = open_file(dataset)
+pep_padded = padding(peptides, long_pep)
+train_data, test_data = split_data(dataset)
+
+model = LSTMPeptides(long_pep, args.hidden_size, args.output_size, args.batch_size, args.layers)
+
+train(train_data, model, args)
+test(test_data, model, long_pep, args.batch_size)
+
+
+
