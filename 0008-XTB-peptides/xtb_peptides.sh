be638d4af8085a916b408301d824806c2468933b
Jarvist Moore Frost <jarvist@gmail.com>
2024-12-17 15:57:47 +0000

    Build a more serious workflow.

--- 0008-XTB-peptides/xtb_peptides.sh
+++ 0008-XTB-peptides/xtb_peptides.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 #
 #iterate through and generate some MD ^)
-
-for peptide in ` cat peptides.dat `
-do
-# B and Z add terminals
-    echo "fab B${peptide}Z; save ${peptide}.pdb" | pymol -c -p
-
+#
 # Hold my beer
 # Simulate protein MD with XTB (empirical tight-binding) model; uses Langevin
 # dynamics and an implicit water model
 # xtb --gfn 2 --md ${peptide}.pdb
- 
-rm mdrestart xtbmdok gfnff_topo xtbtopo.mol
 
+
+for peptide in ` cat peptides.dat `
+do
+
+mkdir -p ${peptide}
+cd ${peptide}
+
+
+# B and Z add terminals
+   echo "fab B${peptide}Z; save ${peptide}.pdb" | pymol -c -p
+   charge=` echo ${peptide} | grep -o '[RHK]' | wc -l ` # count number of cation residues
+
+   echo "Peptide: ${peptide} Inferred charge: ${charge}"
+
+for solvent in "water" "octanol" "woctanol"
+do
+
+mkdir -p md-${solvent}
+cd md-${solvent}
+
+rm mdrestart xtbmdok gfnff_topo xtbtopo.mol
 # Vaguely following protocol in
 # Quantum chemical molecular dynamics and metadynamics simulation of aluminium
 # binding to amyloid-β and related peptides
@@ -38,16 +52,21 @@ cat > md.input << EOF
 \$end
 EOF
 
-xtb --silent --gfn 2 --omd -I md.input ${peptide}.pdb
+xtb  --gfn 2 --omd -I md.input --chrg ${charge} ../${peptide}.pdb > xtb_md.log
 
-# obabel -ixyz xtb.trj -O last_frame.xyz -l1 #not the last frame?
+cd ..
+
+mkdir -p metadynamics-${solvent}
+cd metadynamics-${solvent}
+# bring restart file (MD velocities and positions)
+cp -a ../md-${solvent}/mdrestart ./
 
 cat > metadynamics.input << EOF
 \$md
    restart=true
 
    temp=300.0
-   time=50.0        # simulation length (ps)
+   time=100.0        # simulation length (ps)
    step=4.0        # timestep (fs)
    dump=50.0       # Save coordinates every ... (fs)
    shake=2         # SHAKE on all non-metal bonds
@@ -73,10 +92,19 @@ cat > metadynamics.input << EOF
 \$end
 EOF
 
-xtb --gfn 2 --md --silent -I metadynamics.input ${peptide}.pdb
+xtb --gfn 2 --md -I metadynamics.input --chrg ${charge} ../${peptide}.pdb > xtb_metadynamics.log
 
 #rm mdrestart xtbmdok gfnff_topo xtbtopo.mol
 # xtb --silent --gfn 2  -I control.input 
- # --gfn 2
+# --gfn 2
+
+cd ..
+
+done
+
+
+
+cd ..
+
 done
 
